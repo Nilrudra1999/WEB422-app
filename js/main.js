b4e07aaf2ed10c032ed266adfc83f16173840603
Nilrudra1999 <nilrudram@gmail.com>
2025-01-30 16:09:42 -0500

    start of a2 - part 1 or WEB422-app

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Nilrudra Mukhopadhyay Student ID: 134061175 Date: 16/01/2025
+*  Vercel Link: https://web-assignment1-ceb6-wptfbyec4-nilrudra-mukhopadhyays-projects.vercel.app/api/movies
+*
+********************************************************************************/
