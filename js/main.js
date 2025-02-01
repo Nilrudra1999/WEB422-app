2284456cae0ef007cd0c7bd130462412947b6dbd
Nilrudra1999 <nilrudram@gmail.com>
2025-02-01 01:03:49 -0500

    a2 assignment api connection strong, data being rendered

--- js/main.js
+++ js/main.js
@@ -5,26 +5,65 @@
 *  (including web sites) or distributed to other students.
 * 
 *  Name: Nilrudra Mukhopadhyay Student ID: 134061175 Date: 16/01/2025
-*  Vercel Link: https://web-assignment1-ceb6-wptfbyec4-nilrudra-mukhopadhyays-projects.vercel.app/api/movies
+*  Vercel API Link: https://web-assignment1-ceb6.vercel.app/api/movies
 *
 ********************************************************************************/
 let page = 1;
 const perPage = 10;
 
 // Main function for API data retrival and data rendering---------------------------------------------------------------
-async function loadMovieData(title = null) {
+function loadMovieData(title = null) {
     const baseURL = `https://web-assignment1-ceb6.vercel.app/api/movies`;
-    let url = +title 
-        ? `${baseURL}?page=${page}&perPage=${perPage}&title=${encodeURIComponent(title)}` 
+    let url = title 
+        ? `${baseURL}?page=${page}&perPage=${perPage}&title=${encodeURIComponent(title)}`
         : `${baseURL}?page=${page}&perPage=${perPage}`;
-
+    title 
+        ? document.querySelector('.pagination').classList.add('d-none')
+        : document.querySelector('.pagination').classList.remove('d-none');
     fetch(url)
     .then((res) => res.json())
     .then((data) => {
         console.log(data);
+        renderData(data);
+        updateCurrentPage();        
     })
     .catch((err) => console.error('Error fetching data:', err));
 };
+
+
+
+// Rendering data (array) and other helper functions -------------------------------------------------------------------
+function renderData(data) {
+    console.log('data rendering...');   // using array map and template literal
+    const tbodyContent = data.map(movie => `
+        <tr id="${movie._id}">
+            <td>${movie.year}</td>
+            <td>${movie.title}</td>
+            <td>${movie.plot || 'N/A'}</td>
+            <td>${movie.rated ? movie.rated.toUpperCase() : 'N/A'}</td>
+            <td>${parseRuntime(movie.runtime)}</td>
+        </tr>
+    `).join('');
+    const tbody = document.querySelector('tbody');
+    if (tbody) {
+        tbody.innerHTML = tbodyContent;
+    }
+};
+
+// Helper function for the rendering helper, used to parse movie runtime values
+function parseRuntime(runtime) {
+    if (runtime === undefined) return '';
+    let hours = Math.floor(runtime / 60);
+    let minutes = runtime % 60;
+    minutes = minutes.toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
+
+// Rendering the page number for each page
+function updateCurrentPage() {
+    document.getElementById('current-page').textContent = `${page}`;
+}
+
 
 
 // Loading initial movie data onto the page
