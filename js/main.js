89acfdc35995e171abfc7910bab6cddad89656a2
Nilrudra1999 <nilrudram@gmail.com>
2025-02-01 20:23:29 -0500

    a2 adding event listeners

--- js/main.js
+++ js/main.js
@@ -11,9 +11,10 @@
 let page = 1;
 const perPage = 10;
 
-// Main function for API data retrival and data rendering---------------------------------------------------------------
+// Main function for API data retrival and data rendering ----------------------------------------------------------------------
+// Fetchs data from the API vercel deployment and uses helper functions to render that data
 function loadMovieData(title = null) {
-    const baseURL = `https://web-assignment1-ceb6.vercel.app/api/movies`;
+    const baseURL = `https://web-assignment1-ceb6.vercel.app/api/movies`;   // Loading The Data
     let url = title 
         ? `${baseURL}?page=${page}&perPage=${perPage}&title=${encodeURIComponent(title)}`
         : `${baseURL}?page=${page}&perPage=${perPage}`;
@@ -23,20 +24,22 @@ function loadMovieData(title = null) {
     fetch(url)
     .then((res) => res.json())
     .then((data) => {
-        console.log(data);
-        renderData(data);
-        updateCurrentPage();        
+        console.log(data);      // debugging data check
+        renderData(data);       // creating the <tr> Elements
+        updateCurrentPage();    // Updating the "Current Page"
+        addClickEvent(data);    // Adding Click Events        
     })
     .catch((err) => console.error('Error fetching data:', err));
 };
 
 
 
-// Rendering data (array) and other helper functions -------------------------------------------------------------------
+// Rendering data (array) and other helper functions ---------------------------------------------------------------------------
+// Renders the rows of the table using the data from the API
 function renderData(data) {
     console.log('data rendering...');   // using array map and template literal
     const tbodyContent = data.map(movie => `
-        <tr id="${movie._id}">
+        <tr data-id="${movie._id}">
             <td>${movie.year}</td>
             <td>${movie.title}</td>
             <td>${movie.plot || 'N/A'}</td>
@@ -46,27 +49,82 @@ function renderData(data) {
     `).join('');
     const tbody = document.querySelector('tbody');
     if (tbody) {
-        tbody.innerHTML = tbodyContent;
+        tbody.innerHTML = tbodyContent; // Adding <tr> Elements to the Table
     }
 };
 
-// Helper function for the rendering helper, used to parse movie runtime values
+// Helper function for renderData(), used to parse movie runtime values
 function parseRuntime(runtime) {
     if (runtime === undefined) return '';
     let hours = Math.floor(runtime / 60);
     let minutes = runtime % 60;
     minutes = minutes.toString().padStart(2, '0');
     return `${hours}:${minutes}`;
-}
+};
 
-// Rendering the page number for each page
+// Rendering the page number for each page, Updating the "Current Page"
 function updateCurrentPage() {
     document.getElementById('current-page').textContent = `${page}`;
-}
+};
+
+// Adds click events to each row of the newly created table so that movie "details" can be shown
+function addClickEvent(apiData) {
+    const tableRows = document.querySelectorAll('tr');
+    console.log(tableRows); // Debugging check
+    tableRows.forEach((row) => {
+        if (row.getAttribute('data-id')) {
+            row.addEventListener('click', () => {
+                const data = apiData.find(item => item._id === row.dataset.id);
+                displayMovieDetails(data);
+            });
+        }
+    });
+};
+
+// Helper function for addClickEvent() renders a modal window each time user clicks on a table's row 
+function displayMovieDetails(details) {
+    console.log(details);   // Debugging check
+    const modelTitle = document.querySelector('.modal-title');
+    modelTitle.innerHTML = details.title;
+    let htmlContent = `
+            <img class="img-fluid w-100" src="${details.poster}}"/><br>
+            <br><strong>Directed By:</strong> ${details.directors.join(', ')}<br>
+            <br>${details.fullplot || 'Full plot not avaiable.'}<br>
+            <br><strong>Cast:</strong> ${details.cast.join(', ') || 'N/A'}.<br>
+            <br><strong>Awards:</strong> ${details.awards.text}<br>
+            <br><strong>IMDB Rating:</strong> ${details.imdb.rating}<br>
+            <br><strong>IMDB Votes:</strong> ${details.imdb.votes}<br>
+        <br>`;
+    const modal = document.querySelector('#detailsModal .modal-body');
+    modal.innerHTML = htmlContent;
+    let myModal = new bootstrap.Modal(document.getElementById('detailsModal'), {
+        backdrop: 'static', 
+        keyboard: false,
+        focus: true,
+    });
+    myModal.show();
+};
 
 
 
-// Loading initial movie data onto the page
+// Event listeners with functions to render the page ---------------------------------------------------------------------------
+// Launches the loadMovieData() function each time the page refreshes or during first launch
 document.addEventListener("DOMContentLoaded", function () {
     loadMovieData();
+});
+
+// Loads the previous page using loadMovieData() when user clicks on the previous pagination button
+document.getElementById('previous-page').addEventListener('click', () => {
+    if (page > 1) {
+        page = page - 1;
+        loadMovieData();
+    }
+});
+
+// Loads the next page using loadMovieData() when user clicks on the next pagination button
+document.getElementById('next-page').addEventListener('click', () => {
+    if (page > 0 || page < 10) {
+        page = page + 1;
+        loadMovieData();
+    }
 });
