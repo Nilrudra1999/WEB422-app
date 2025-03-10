798f2acf84157ef91baad7cf90047d90ecb9d188
Nilrudra1999 <nilrudram@gmail.com>
2025-03-10 12:24:23 -0400

    a4 complete new project

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,7 @@
-/*********************************************************************************
-*  WEB422 – Assignment 3
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: Nilrudra Mukhopadhyay Student ID: 134061175 Date: 01/02/2025
-*  Vercel API Link: https://web-assignment1-ceb6.vercel.app/api/movies
-*  Vercel App Link:
-*
-********************************************************************************/
 import "@/styles/globals.css";
-import Layout from "@/components/Layout";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { SWRConfig } from 'swr';
+import '@/styles/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
-    return (
-        <SWRConfig value={{ fetcher: (...args) => fetch(...args).then((res) => res.json()) }}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </SWRConfig>
-    );
-};
+    return <Component {...pageProps} />;
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,3 @@
-/*********************************************************************************
-*  WEB422 – Assignment 3
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: Nilrudra Mukhopadhyay Student ID: 134061175 Date: 01/02/2025
-*  Vercel API Link: https://web-assignment1-ceb6.vercel.app/api/movies
-*  Vercel App Link:
-*
-********************************************************************************/
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -16,9 +5,9 @@ export default function Document() {
         <Html lang="en">
             <Head />
             <body>
-                <Main />        {/* Renders the page content */}
-                <NextScript />  {/* Loads Next.js scripts */}
+                <Main />
+                <NextScript />
             </body>
         </Html>
     );
-};
+}
