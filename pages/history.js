3e746a841828b3f72ab586b37d6fe93e40c84fc0
Nilrudra1999 <nilrudram@gmail.com>
2025-03-19 20:09:47 -0400

    a5 adding formal declaration

--- pages/history.js
+++ pages/history.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB422 – Assignment 5
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Nilrudra Mukhopadhyay   Student ID: 134061175   Date: 03/25/2025
+*
+*
+********************************************************************************/ 
 import { useAtom } from 'jotai';
 import { useRouter } from 'next/router';
 import { searchHistoryAtom } from '@/store';
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB422 – Assignment 5
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Nilrudra Mukhopadhyay   Student ID: 134061175   Date: 03/25/2025
+*
+*
+********************************************************************************/ 
 import { Image } from "react-bootstrap";
 import { Col, Container, Row } from "react-bootstrap";
 
